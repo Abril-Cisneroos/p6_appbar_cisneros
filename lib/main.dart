2d4bae220edb117f7bcd4086217ad241baeffffb
FRIDA_ABRIL_CISNEROS_HERNANDEZ <a.22308051281162@cbtis128.edu.mx>
2025-02-19 20:33:22 +0000

    enviar practica 6

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Cisneros flutter',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
@@ -17,25 +18,43 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Cisneros AppBar'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
+    var appBar2 = AppBar(
+      actions: [
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {}, // l que esta entre llaves son acciones
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.more_vert,
+          ),
+          onPressed: () {},
+        )
+      ],
+
+      backgroundColor: Color(0xffc296ca),
+
+      // The title text which will be shown on the action bar
+      title: Text(title),
+      leading: const Icon(Icons.access_time),
+      centerTitle: true,
+    );
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
+      appBar: appBar2,
       body: Center(
         child: Text(
-          'Hello, World!',
+          'Hola Frida Abril Cisneros Hernandez con AppBar',
         ),
       ),
     );
